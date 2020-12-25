c86f109bae6ee4ce7a7937706ddf7a04abfee2b4
unknown <jhonjlinaresb@gmail.com>
2020-12-25 20:13:08 +0100

    Add Header component and include of the index

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom'
+import './App.css';
+//import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+//import Home from './containers/Home/Home';
+//import Login from './containers/Login/Login';
+//import Logout from './containers/Logout/Logout';
+//import 'antd/dist/antd.css';
+//import Register from './containers/Register/Register';
+//import axios from 'axios';
+//import Profile from './containers/Profile/Profile';
+//import Appointments from './containers/Appointments/Appointments';
+//import PrivateZone from './guards/PrivateZone';
+//import Error404 from './containers/Error404/Error404.jsx';
+//import UserList from './containers/UserList/UserList';
+//import CheckPrivileges from './guards/CheckPrivileges';
+function App() {
+/* {
+  console.log(process.env.REACT_APP_BASE_URL);
+  let initialUser = null;
+  try {
+    console.log(localStorage.getItem('user'));
+    initialUser = JSON.parse(localStorage.getItem('user'));
+  } catch (error) {
+    console.error(error);
+  }
+  const [user, setUser] = useState(initialUser);
+  useEffect(() => {
+    const token = localStorage.getItem('authToken')
+    axios.get(process.env.REACT_APP_BASE_URL + '/users',
+      {
+        headers: {
+          Authorization: token
+        }
+      })
+      .then(res => {
+        setUser(res.data)
+      })
+  }, []) */
+  return (
+    <BrowserRouter>
+    
+      <Header />
+      
+    </BrowserRouter >
+    );
+  }
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+  
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
+
+// const Header = (props) => {
+    const Header = ({user,setUser}) => {
+    const logout = () => {
+        localStorage.clear();
+        // props.setUser(null)
+        setUser(null)
+    }
+    return (
+        <header className="header">
+            <Link to="/">Home</Link>
+            {/* {props.user ? */}
+            {console.log(user)}
+            {user ?
+                <div className="loggedIn">
+                    {['admin'].includes(user.role) &&<Link to="/users">users</Link> }
+                    {/* {['doctores','becarios'].includes(user.role) &&<Link to="/pacientes">pacientes</Link> } */}
+                    <Link to="/profile">{user?.email} - {user?.role}Profile</Link>
+                    <a href="/" ><span className="logout" onClick={logout}>Logout</span></a>
+                </div> :
+                <div className="notLoggedIn">
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </div>}
+
+        </header>
+    )
+}
+export default Header; 
